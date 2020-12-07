6161a5563709786d180fb1e6868a98b9f80006a8
Mustafa Alper Sayan <alper.sayan@ozu.edu.tr>
2020-12-07 18:29:55 +0300

    Update saloonScript.js
    Construct saloons

--- saloonScript.js
+++ saloonScript.js
@@ -14,3 +14,19 @@ for (item in sessionStorage) {
     mylist.push(sessionStorage[item]);
   }
 }
+
+for (item in mylist) {
+    x = JSON.parse(mylist[item]);
+    if(x.saloons == "Salon 1"){
+        saloon_one_string = x.name;
+    }else if(x.saloons == "Salon 2"){
+        saloon_two_string = x.name;
+    }else if(x.saloons == "Salon 3"){
+        saloon_three_string = x.name;
+    }else if(x.saloons == "Salon 4"){
+        saloon_four_string = x.name;
+    }else if(x.saloons == "Salon 5"){
+        saloon_five_string = x.name;
+    }
+
+  }
