e16389e31dfea01dc9694826064f31aca58593fd
dogukan1106 <56073555+dogukan1106@users.noreply.github.com>
2020-12-17 19:05:15 +0300

    new design for customer page

--- saloons/sal_script.js
+++ saloons/sal_script.js
@@ -1,229 +1,209 @@
+var seatList = [];
 
+function changeData() {
+  buttontype = document.getElementById('sub').name;
+  validator = validateForm();
+  if (buttontype === 'buttonadd_seat_one') {
+    if (validator) {
+      let seat_name = document.forms['AdminForm']['koltuk'].value;
+      let seat_no = document.forms['AdminForm']['numara'].value;
+      let name_surname = document.forms['AdminForm']['name_surname'].value;
 
+      let seat_val = seat_name + seat_no;
 
+      localStorage.setItem(
+        localStorage.length,
+        JSON.stringify({
+          saloon_type: 'Saloon 1',
+          seat: seat_val,
+          customer: name_surname,
+        })
+      );
+      console.log({
+        saloon_type: 'Saloon 1',
+        seat: seat_val,
+        customer: name_surname,
+      });
 
-
-
-
-
-function changeData(){
-
-  
-    buttontype = document.getElementById("sub").name;
-    validator = validateForm();
-    if (buttontype === "buttonadd_seat_one") {
-      if (validator) {
-
-        let seat_name = document.forms["AdminForm"]["koltuk"].value;
-      let seat_no = document.forms["AdminForm"]["numara"].value;
-      let name_surname = document.forms["AdminForm"]["name_surname"].value;
-  
-      let seat_val = seat_name+seat_no;
-  
-        localStorage.setItem(
-          localStorage.length,
-          JSON.stringify({
-            saloon_type: "Saloon 1",
-            seat: seat_val,
-            customer: name_surname,
-          })
-        );
-        console.log({
-          saloon_type: "Saloon 1",
-            seat: seat_val,
-            customer: name_surname,
-        });
-  
-        myobject = JSON.parse(localStorage.getItem(localStorage.length - 1));
-        window.alert("Seat has been assigned");
-      }
-    } else if(buttontype === "buttonadd_seat_two"){
-      if (validator) {
-
-        let seat_name = document.forms["AdminForm"]["koltuk"].value;
-      let seat_no = document.forms["AdminForm"]["numara"].value;
-      let name_surname = document.forms["AdminForm"]["name_surname"].value;
-  
-      let seat_val = seat_name+seat_no;
-  
-        localStorage.setItem(
-          localStorage.length,
-          JSON.stringify({
-            saloon_type: "Saloon 2",
-            seat: seat_val,
-            customer: name_surname,
-          })
-        );
-        console.log({
-          saloon_type: "Saloon 2",
-            seat: seat_val,
-            customer: name_surname,
-        });
-  
-        myobject = JSON.parse(localStorage.getItem(localStorage.length - 1));
-        window.alert("Seat has been assigned");
-      }
-
-
-    }else if(buttontype === "buttonadd_seat_three"){
-      if (validator) {
-
-        let seat_name = document.forms["AdminForm"]["koltuk"].value;
-      let seat_no = document.forms["AdminForm"]["numara"].value;
-      let name_surname = document.forms["AdminForm"]["name_surname"].value;
-  
-      let seat_val = seat_name+seat_no;
-  
-        localStorage.setItem(
-          localStorage.length,
-          JSON.stringify({
-            saloon_type: "Saloon 3",
-            seat: seat_val,
-            customer: name_surname,
-          })
-        );
-        console.log({
-          saloon_type: "Saloon 3",
-            seat: seat_val,
-            customer: name_surname,
-        });
-  
-        myobject = JSON.parse(localStorage.getItem(localStorage.length - 1));
-        window.alert("Seat has been assigned");
-      }
-
-
-    }else if(buttontype === "buttonadd_seat_four"){
-      if (validator) {
-
-        let seat_name = document.forms["AdminForm"]["koltuk"].value;
-      let seat_no = document.forms["AdminForm"]["numara"].value;
-      let name_surname = document.forms["AdminForm"]["name_surname"].value;
-  
-      let seat_val = seat_name+seat_no;
-  
-        localStorage.setItem(
-          localStorage.length,
-          JSON.stringify({
-            saloon_type: "Saloon 4",
-            seat: seat_val,
-            customer: name_surname,
-          })
-        );
-        console.log({
-          saloon_type: "Saloon 4",
-            seat: seat_val,
-            customer: name_surname,
-        });
-  
-        myobject = JSON.parse(localStorage.getItem(localStorage.length - 1));
-        window.alert("Seat has been assigned");
-      }
-
-
-    }else if(buttontype === "buttonadd_seat_five"){
-      if (validator) {
-
-        let seat_name = document.forms["AdminForm"]["koltuk"].value;
-      let seat_no = document.forms["AdminForm"]["numara"].value;
-      let name_surname = document.forms["AdminForm"]["name_surname"].value;
-  
-      let seat_val = seat_name+seat_no;
-  
-        localStorage.setItem(
-          localStorage.length,
-          JSON.stringify({
-            saloon_type: "Saloon 5",
-            seat: seat_val,
-            customer: name_surname,
-          })
-        );
-        console.log({
-          saloon_type: "Saloon 5",
-            seat: seat_val,
-            customer: name_surname,
-        });
-  
-        myobject = JSON.parse(localStorage.getItem(localStorage.length - 1));
-        window.alert("Seat has been assigned");
-      }
-
-
+      myobject = JSON.parse(localStorage.getItem(localStorage.length - 1));
+      window.alert('Seat has been assigned');
     }
+  } else if (buttontype === 'buttonadd_seat_two') {
+    if (validator) {
+      let seat_name = document.forms['AdminForm']['koltuk'].value;
+      let seat_no = document.forms['AdminForm']['numara'].value;
+      let name_surname = document.forms['AdminForm']['name_surname'].value;
+
+      let seat_val = seat_name + seat_no;
+
+      localStorage.setItem(
+        localStorage.length,
+        JSON.stringify({
+          saloon_type: 'Saloon 2',
+          seat: seat_val,
+          customer: name_surname,
+        })
+      );
+      console.log({
+        saloon_type: 'Saloon 2',
+        seat: seat_val,
+        customer: name_surname,
+      });
+
+      myobject = JSON.parse(localStorage.getItem(localStorage.length - 1));
+      window.alert('Seat has been assigned');
+    }
+  } else if (buttontype === 'buttonadd_seat_three') {
+    if (validator) {
+      let seat_name = document.forms['AdminForm']['koltuk'].value;
+      let seat_no = document.forms['AdminForm']['numara'].value;
+      let name_surname = document.forms['AdminForm']['name_surname'].value;
+
+      let seat_val = seat_name + seat_no;
+
+      localStorage.setItem(
+        localStorage.length,
+        JSON.stringify({
+          saloon_type: 'Saloon 3',
+          seat: seat_val,
+          customer: name_surname,
+        })
+      );
+      console.log({
+        saloon_type: 'Saloon 3',
+        seat: seat_val,
+        customer: name_surname,
+      });
+
+      myobject = JSON.parse(localStorage.getItem(localStorage.length - 1));
+      window.alert('Seat has been assigned');
+    }
+  } else if (buttontype === 'buttonadd_seat_four') {
+    if (validator) {
+      let seat_name = document.forms['AdminForm']['koltuk'].value;
+      let seat_no = document.forms['AdminForm']['numara'].value;
+      let name_surname = document.forms['AdminForm']['name_surname'].value;
+
+      let seat_val = seat_name + seat_no;
+
+      localStorage.setItem(
+        localStorage.length,
+        JSON.stringify({
+          saloon_type: 'Saloon 4',
+          seat: seat_val,
+          customer: name_surname,
+        })
+      );
+      console.log({
+        saloon_type: 'Saloon 4',
+        seat: seat_val,
+        customer: name_surname,
+      });
+
+      myobject = JSON.parse(localStorage.getItem(localStorage.length - 1));
+      window.alert('Seat has been assigned');
+    }
+  } else if (buttontype === 'buttonadd_seat_five') {
+    if (validator) {
+      let seat_name = document.forms['AdminForm']['koltuk'].value;
+      let seat_no = document.forms['AdminForm']['numara'].value;
+      let name_surname = document.forms['AdminForm']['name_surname'].value;
+
+      let seat_val = seat_name + seat_no;
+
+      localStorage.setItem(
+        localStorage.length,
+        JSON.stringify({
+          saloon_type: 'Saloon 5',
+          seat: seat_val,
+          customer: name_surname,
+        })
+      );
+      console.log({
+        saloon_type: 'Saloon 5',
+        seat: seat_val,
+        customer: name_surname,
+      });
+
+      myobject = JSON.parse(localStorage.getItem(localStorage.length - 1));
+      window.alert('Seat has been assigned');
+    }
+  }
 }
 
+function validateForm() {
+  // buttontype = document.getElementById("sub").name;
 
+  let seat_name = document.forms['AdminForm']['koltuk'];
+  let seat_no = document.forms['AdminForm']['numara'];
+  let name_surname = document.forms['AdminForm']['name_surname'];
 
-
-
-
-
-  function validateForm() {
-    // buttontype = document.getElementById("sub").name;
-  
-
-      let seat_name = document.forms["AdminForm"]["koltuk"];
-      let seat_no = document.forms["AdminForm"]["numara"];
-      let name_surname = document.forms["AdminForm"]["name_surname"];
-  
-  
-      if (seat_name.value == "") {
-        window.alert("Please select a seat name.");
-        seat_name.focus();
-        return false;
-      }
-  
-      if (seat_no.value == "") {
-        window.alert("Please select a valid seat number.");
-        seat_no.focus();
-        return false;
-      }
-  
-      if (name_surname.value == "") {
-        window.alert("Please enter your name and surname to register for a seat.");
-        time.focus();
-        return false;
-      }
-  
-      return true;
-
-
-
-
-    
+  if (seat_name.value == '') {
+    window.alert('Please select a seat name.');
+    seat_name.focus();
+    return false;
   }
 
-document.getElementById("main").style.display = "grid";
-document.getElementById("main").style.gridAutoFlow = "column";
-document.getElementById("main").style.justifyItems = "center";
-document.getElementById("main").style.gridColumnGap = "5rem";
+  if (seat_no.value == '') {
+    window.alert('Please select a valid seat number.');
+    seat_no.focus();
+    return false;
+  }
 
+  if (name_surname.value == '') {
+    window.alert('Please enter your name and surname to register for a seat.');
+    time.focus();
+    return false;
+  }
 
-document.getElementById("addForm").style.width = "350px";
-document.getElementById("addForm").style.height = "300px";
-document.getElementById("addForm").style.backgroundColor =
-  "rgba(252, 87, 87, 0.8)";
-document.getElementById("addForm").style.border = "0.35em solid";
-document.getElementById("addForm").style.borderRadius = "20px";
-document.getElementById("addForm").style.display = "grid";
-document.getElementById("addForm").style.gridTemplateColumns = "auto";
-document.getElementById("addForm").style.gridTemplateRows =
-  "50px 50px 50px 50px 50px";
-document.getElementById("addForm").style.justifyContent = "center";
-document.getElementById("addForm").style.justifyItems = "end";
-document.getElementById("addForm").style.alignItems = "center";
+  return true;
+}
 
-document.getElementById("nicelist").style.width = "500px";
-document.getElementById("nicelist").style.height = "300px";
-document.getElementById("nicelist").style.backgroundColor =
-  "rgba(252, 87, 87, 0.8)";
-document.getElementById("nicelist").style.border = "0.35em solid";
-document.getElementById("nicelist").style.borderRadius = "20px";
-document.getElementById("nicelist").style.display = "grid";
-document.getElementById("nicelist").style.gridTemplateColumns = "auto";
-document.getElementById("nicelist").style.gridTemplateRows =
-  "50px 50px 50px 50px 50px";
-document.getElementById("nicelist").style.justifyContent = "center";
-document.getElementById("nicelist").style.justifyItems = "end";
-document.getElementById("nicelist").style.alignItems = "center";
+document.getElementById('main').style.display = 'grid';
+document.getElementById('main').style.gridAutoFlow = 'column';
+document.getElementById('main').style.justifyItems = 'center';
+document.getElementById('main').style.gridColumnGap = '5rem';
 
+document.getElementById('addForm').style.width = '350px';
+document.getElementById('addForm').style.height = '300px';
+document.getElementById('addForm').style.backgroundColor =
+  'rgba(252, 87, 87, 0.8)';
+document.getElementById('addForm').style.border = '0.35em solid';
+document.getElementById('addForm').style.borderRadius = '20px';
+document.getElementById('addForm').style.display = 'grid';
+document.getElementById('addForm').style.gridTemplateColumns = 'auto';
+document.getElementById('addForm').style.gridTemplateRows =
+  '50px 50px 50px 50px 50px';
+document.getElementById('addForm').style.justifyContent = 'center';
+document.getElementById('addForm').style.justifyItems = 'end';
+document.getElementById('addForm').style.alignItems = 'center';
+
+document.getElementById('nicelist').style.width = '500px';
+document.getElementById('nicelist').style.height = '300px';
+document.getElementById('nicelist').style.backgroundColor =
+  'rgba(252, 87, 87, 0.8)';
+document.getElementById('nicelist').style.border = '0.35em solid';
+document.getElementById('nicelist').style.borderRadius = '20px';
+document.getElementById('nicelist').style.display = 'grid';
+document.getElementById('nicelist').style.gridTemplateColumns = 'auto';
+document.getElementById('nicelist').style.gridTemplateRows =
+  '50px 50px 50px 50px 50px';
+document.getElementById('nicelist').style.justifyContent = 'center';
+document.getElementById('nicelist').style.justifyItems = 'end';
+document.getElementById('nicelist').style.alignItems = 'center';
+
+function chooseSeat(buttonId) {
+  color = window.getComputedStyle(document.getElementById(buttonId))
+    .backgroundColor;
+  id = document.getElementById('perde').getAttribute('name').toString();
+  console.log(id);
+  if (color == 'rgb(0, 128, 0)') {
+    globalThis.seatList.push([id, buttonId]);
+    console.log(seatList);
+    document.getElementById(buttonId).style.backgroundColor = 'red';
+  } else {
+    globalThis.seatList.pop([id, buttonId]);
+    console.log(seatList);
+    document.getElementById(buttonId).style.backgroundColor = 'rgb(0, 128, 0)';
+  }
+}
